c899683124a07838828b73a335024f09d6ebc740
nassery318 <nassery318@gmail.com>
2025-08-26 11:51:53 -0400

    added dynamic extendable sidebar for better UI

--- apps/frontend/src/components/ui/app-sidebar.tsx
+++ apps/frontend/src/components/ui/app-sidebar.tsx
@@ -1,16 +1,25 @@
-
-import { LayoutDashboard, Unplug, Send, HousePlug, ChartNoAxesCombined } from "lucide-react";
+import {
+  LayoutDashboard,
+  Unplug,
+  Send,
+  HousePlug,
+  ChartNoAxesCombined,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-react";
 import { setConnected as valkeySetConnected } from "@/state/valkey-features/connection/connectionSlice.ts";
 import { selectConnected } from "@/state/valkey-features/connection/connectionSelectors.ts";
 import { useSelector } from "react-redux";
 import { useAppDispatch } from "@/hooks/hooks";
 import { useNavigate, Link, useLocation } from "react-router";
+import { useState } from "react";
 
 export function AppSidebar() {
   const isConnected = useSelector(selectConnected);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const location = useLocation();
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const handleDisconnect = () => {
     dispatch(valkeySetConnected(false));
@@ -24,65 +33,90 @@ export function AppSidebar() {
   };
 
   return (
-    <nav className="bg-white w-18 h-screen p-4 shadow border-1 flex flex-col justify-between items-center">
-      <div className="flex items-center flex-col">
+    <nav
+      className={`bg-white ${
+        isExpanded ? "w-52 items-start" : "w-18 items-center"
+      } h-screen p-4 shadow-lg border-r-2 flex flex-col justify-between transition-all duration-300 items-center relative`}
+    >
+      <div
+        className={`flex ${
+          isExpanded ? "items-start" : "items-center"
+        }  flex-col`}
+      >
         {/* Header */}
-        <div className="" title="Skyscope">
+        <div className="flex items-center" title="Skyscope">
           <img src="../../assets/img/logo.png" alt="logo" className="h-8" />
+          {isExpanded && (
+            <span className="ml-3 font-bold text-lg">Skyscope</span>
+          )}
         </div>
 
         {/* menu items */}
         {isConnected && (
           <div className="mt-10">
-            <ul className="space-y-4">
-            <li>
+            <ul className="space-y-2">
+              <li>
                 <Link
                   to="/connect"
-                  className={`flex items-center justify-center p-2 ${getNavItemClasses('/connect')}`}
+                  className={`flex p-2 ${getNavItemClasses("/connect")}`}
                   title="Connections"
                 >
                   <HousePlug size={22} />
+                  {isExpanded && <span className="ml-3">Connections</span>}
                 </Link>
               </li>
               <li>
                 <Link
                   to="/dashboard"
-                  className={`flex items-center justify-center p-2 ${getNavItemClasses('/dashboard')}`}
+                  className={`flex p-2 ${getNavItemClasses("/dashboard")}`}
                   title="Dashboard"
                 >
                   <LayoutDashboard size={22} />
+                  {isExpanded && <span className="ml-3">Dashboard</span>}
                 </Link>
               </li>
               <li>
                 <Link
                   to="/sendcommand"
-                  className={`flex items-center justify-center p-2 ${getNavItemClasses("/sendcommand")}`}
+                  className={`flex p-2 ${getNavItemClasses("/sendcommand")}`}
                   title="Send Command"
                 >
                   <Send size={22} />
+                  {isExpanded && <span className="ml-3">Send Command</span>}
                 </Link>
               </li>
               <li>
                 <Link
                   to="/"
-                  className={`flex items-center justify-center p-2 ${getNavItemClasses("/")}`}
+                  className={`flex p-2 ${getNavItemClasses("/")}`}
                   title="Monitoring"
                 >
                   <ChartNoAxesCombined size={22} />
+                  {isExpanded && <span className="ml-3">Monitoring</span>}
                 </Link>
               </li>
             </ul>
           </div>
         )}
       </div>
+      {/* expand/collapse sidebar */}
+      <button
+        onClick={() => setIsExpanded(!isExpanded)}
+        className="p-1 cursor-pointer rounded-full bg-white border-2 text-tw-primary hover:bg-tw-primary hover:text-white absolute top-1/2 right-[-14px] z-10"
+        title={isExpanded ? "Collapse sidebar" : "Expand sidebar"}
+      >
+        {isExpanded ? <ChevronLeft size={15} /> : <ChevronRight size={15} />}
+      </button>
+
       {/* disconnect */}
       {isConnected && (
         <button
           onClick={handleDisconnect}
-          className="cursor-pointer p-1 rounded bg-tw-primary text-white hover:text-gray-200"
+          className="w-full flex p-2 rounded text-tw-primary border-1 border-tw-primary hover:bg-tw-primary hover:text-white"
           title="Disconnect"
         >
-          <Unplug size={25} />
+          <Unplug size={22} />
+          {isExpanded && <span className="ml-3">Disconnect</span>}
         </button>
       )}
     </nav>
